6203ff1edfaecbb7f6a28ef0913991ac59848eb6
Evans Sese <sese_evans@yahoo.com>
2023-08-01 07:47:46 -0700

    [Feature] Task 0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,13 +1,2 @@
 #!/usr/bin/env ruby
-def match_school(input)
-  pattern = /School/
-  matches = input.scan(pattern)
-  puts matches.join
-end
-# Get the input argument from the command line
-if ARGV.length != 1
-  puts 'Usage: ruby script_name.rb <input>'
-  exit 1
-end
-input_argument = ARGV[0]
-match_school(input_argument)
+puts ARGV[0].scan(/School/).join
