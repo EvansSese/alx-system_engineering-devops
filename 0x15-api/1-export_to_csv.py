33b1477c5f0983732ead99c6f935207d1f2f5aca
EvansSese <sese_evans@yahoo.com>
2023-10-23 14:52:50 +0300

    [Feature] Task 1

---
+++ 0x15-api/1-export_to_csv.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" Script to fetch data from API """
+
+
+import csv
+import requests
+import sys
+
+
+def get_data():
+    """ Function to get data """
+    employee_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    users_url = f"{url}users/{employee_id}"
+    todos_url = f"{url}todos?userId={employee_id}"
+    try:
+        user = requests.get(users_url)
+        user.raise_for_status()
+        user_json = user.json()
+
+        todos = requests.get(todos_url)
+        todos.raise_for_status()
+        todos_json = todos.json()
+
+        employee_name = user_json["name"]
+        employee_id = user_json["id"]
+        total_tasks = len(todos_json)
+        completed_tasks = sum(1 for task in todos_json if task["completed"])
+
+        print(f"Employee {employee_name} is done with tasks({completed_tasks}"
+              f"/{total_tasks}):")
+        for completed_task in todos_json:
+            if completed_task["completed"]:
+                print(f"\t {completed_task['title']}")
+        csv_file_name = f"{employee_id}.csv"
+        with open(csv_file_name,"w", newline="") as file:
+            csv_writer = csv.writer(file)
+            for task in todos_json:
+                csv_writer.writerow([employee_id, employee_name,
+                                     task["completed"], task["title"]])
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    """ Run the file"""
+    get_data()
