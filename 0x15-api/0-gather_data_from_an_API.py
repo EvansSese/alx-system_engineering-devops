9b558e291880271ac9ef44747af77413459871b6
EvansSese <sese_evans@yahoo.com>
2023-10-23 12:54:04 +0300

    [Feature] Task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Script to fetch data from API """
+
+
+import requests
+import sys
+
+
+employee_id = sys.argv[1]
+
+
+def get_data(employee_id):
+    """ Function to get data"""
+    url = "https://jsonplaceholder.typicode.com/"
+    users_url = f"{url}users/{employee_id}"
+    todos_url = f"{url}todos?userId={employee_id}"
+    try:
+        user = requests.get(users_url)
+        user.raise_for_status()
+        user_json = user.json()
+
+        todos = requests.get(todos_url)
+        todos.raise_for_status()
+        todos_json = todos.json()
+
+        employee_name = user_json["name"]
+        total_tasks = len(todos_json)
+        completed_tasks = sum(1 for task in todos_json if task["completed"])
+
+        print(f"Employee {employee_name} is done with tasks {completed_tasks}"
+              f"/{total_tasks}:")
+        for completed_task in todos_json:
+            if completed_task["completed"]:
+                print(f"\t{completed_task['title']}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    get_data(employee_id)
