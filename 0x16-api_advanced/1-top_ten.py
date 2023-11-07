45440a74887e97ddf1484adbadb119d9217ba88e
EvansSese <sese_evans@yahoo.com>
2023-11-07 16:37:05 +0300

    [Feature] Task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Function to return number of subscribers """
+import requests
+import sys
+
+
+def top_ten(subreddit):
+    """ Function to fetch subs """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {
+        'User-Agent': '0-subs'
+    }
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        try:
+            post_data = response.json()['data']['children'][:10]
+            for post in post_data:
+                title = post['data']['title']
+                print(f"{title}")
+        except (KeyError, ValueError):
+            print(None)
+    else:
+        print(None)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        subreddit = sys.argv[2]
+        top_ten(subreddit)
