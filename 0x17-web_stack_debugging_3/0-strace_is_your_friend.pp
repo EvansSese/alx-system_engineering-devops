80e70b499b3508186025e598df09f4f5228258d5
EvansSese <sese_evans@yahoo.com>
2023-11-08 18:04:41 +0300

    [Feature] Task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixes typo of `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+
+exec { 'fix-typo-ext':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
